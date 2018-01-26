2ce719e9bc201612498255708ad4dc88c75df65f
NIKITA AD <nikita8ad@gmail.com>
2018-01-26 23:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function shout(string)
 {
   return string.toUpperCase()
 }
+
+function whisper(string)
+{
+  return string.toLowerCase()
+}
+
+function logShout(string)
+{
+  Console.log(string.toUpperCase())
+}
+
+function logWhisper(string)
+{
+  Console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  if() string.toUpperCase()
+}
